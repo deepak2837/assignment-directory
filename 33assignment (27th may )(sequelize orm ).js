d71d6c2b97bb43803b353544650acc3766929eb2
deepak2837 <52418151+deepak2837@users.noreply.github.com>
2022-06-03 17:04:18 +0530

    Update 33assignment (27th may )(sequelize orm ).js

--- 33assignment (27th may )(sequelize orm ).js
+++ 33assignment (27th may )(sequelize orm ).js
@@ -1,1 +1,5 @@
 //33 assignment (27th may )(sequelize  orm ).js
+// here is the multiple files 
+// these files are assicioated with the assignment code 
+
+https://github.com/deepak2837/learning/tree/main/ecommerce/assignment%20learning%20
