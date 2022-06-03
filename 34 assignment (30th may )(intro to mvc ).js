fa25857725f321e724bb91948fab22e168e3fe5f
deepak2837 <52418151+deepak2837@users.noreply.github.com>
2022-06-03 17:34:43 +0530

    Update 34 assignment (30th may )(intro to mvc ).js

--- 34 assignment (30th may )(intro to mvc ).js
+++ 34 assignment (30th may )(intro to mvc ).js
@@ -1,1 +1,49 @@
 //34 assignment (30th may )(intro to mvc ).js
+
+
+// question number 1 
+// Design an MVC for a restaurant service to serve customers for lunch and dinner.
+
+// [View]
+Customer [Can view the menu and eats the food] 
+Restaurant Service 
+Order food I Serve food 
+Waiter 
+
+// [Controller] 
+relays food I Sends food 
+Cook 
+
+
+// [Model]
+[Prepare food using raw materials from refrigerator] 
+Model- The Cook and Refrigerator (raw data to be used in the service) Controller- Waiter (Accepts all incoming requests from customers and fulfill the requirements) View- Menu and Food (Final output) 
+P
+
+
+
+
+
+// question number 2nd
+// Design an MVC for a cab booking service like Uber, Ola
+
+// [View]
+Interaction with user Vehicle tracking Estimated Price Destination Selection 
+Cab Booking Service 
+Cab request I Assign Cab 
+Vehicle, Route. Driver & Location Selection ■=0 I Send details 
+
+
+
+
+// [Controller] 
+Payment process 
+GPS tracking
+Price calculation 
+User requests handling 
+
+
+
+// [Model] 
+Vehicle Route 
+Driver Location 
